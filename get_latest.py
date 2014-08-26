d3eae5f8eb887762d54b06ddf7649f35120c696a
Alex Volkov <alex@flamy.ca>
2014-08-26 14:30:27 -0400

    Fixed PEP8 Warnings.

--- get_latest.py
+++ get_latest.py
@@ -9,17 +9,22 @@ import requests
 import codecs
 import sys
 
+
 def get_latest_remote_id(rss_url):
     feed = feedparser.parse(rss_url)
     return int(feed.values()[10][0]['id'].split('/')[-1])
+
+
 def get_latest_local_id(fname):
     return int(open(fname, 'r').read())
+
 
 def download_archive(url, start, stop, step):
     current = start
     buff = u''
     while True:
-        dl_url = "%s/%d/%d" % (url, current, stop if current + step > stop else current + step)
+        dl_url = "%s/%d/%d" % (
+            url, current, stop if current + step > stop else current + step)
         out = requests.get(dl_url)
         if out.ok:
             buff += unicode(out.text)
@@ -29,7 +34,7 @@ def download_archive(url, start, stop, step):
                 current = stop if current + step > stop else current + step
         else:
             return False
-        
+
 gmane_rss = "http://rss.gmane.org/messages/complete/gmane.org.user-groups.linux.tolug"
 latest_id_file = 'latest_msg'
 archive_base_url = 'http://download.gmane.org/gmane.org.user-groups.linux.tolug'
@@ -40,15 +45,20 @@ latest_remote = get_latest_remote_id(gmane_rss)
 latest_local = get_latest_local_id(latest_id_file)
 
 if latest_remote > latest_local:
-    print  latest_local, latest_remote
-    append =  download_archive(archive_base_url, latest_local+1, latest_remote+1, step)
+    print latest_local, latest_remote
+    append = download_archive(
+        archive_base_url,
+        latest_local + 1,
+        latest_remote + 1,
+        step
+    )
     if not append:
         print "error downloading archive"
         sys.exit(2)
-    f_append = codecs.open(datafile, 'a','utf-8')
+    f_append = codecs.open(datafile, 'a', 'utf-8')
     f_append.write(append)
     f_append.close()
-    open(latest_id_file,'w').write(str(latest_remote))
+    open(latest_id_file, 'w').write(str(latest_remote))
 else:
     print "Up to date"
     sys.exit(1)
--- gmain_archive.py
+++ gmain_archive.py
@@ -6,7 +6,7 @@ PHP execution 30 second limit on the server
 '''
 
 import requests
-from time import time, sleep
+from time import time
 import codecs
 import StringIO
 maxget = 57370
@@ -18,29 +18,29 @@ target = codecs.open('tlug_data.txt', 'a', 'utf-8')
 tlug_url = 'http://download.gmane.org/gmane.org.user-groups.linux.tolug'
 
 step = orig_step
+out = None
 while current <= maxget:
-	req_start = time()
-	buff = StringIO.StringIO()
-	dl_url = "%s/%d/%d" % (tlug_url, current, current+step % (maxget + 1))
-	print dl_url
-	out = requests.get(dl_url)
-	if out.ok:
-		buff.write(out.text)
-		del out
-	else:
-		print "Network error, retrying", out
-		del out
-		continue
+    req_start = time()
+    buff = StringIO.StringIO()
+    dl_url = "%s/%d/%d" % (tlug_url, current, current + step % (maxget + 1))
+    print dl_url
+    del out
+    out = requests.get(dl_url)
+    if out.ok:
+        buff.write(out.text)
+    else:
+        print "Network error, retrying", out
+        continue
 
-	interval = time() - req_start
-	print "Interval: ", interval
-	if interval < 30:
-		target.write(buff.getvalue())
-		current += step
-		if step != orig_step:
-			step = int((1.5*step) % orig_step)
-	else:
-		step /= 2
-	buff.close()
+    interval = time() - req_start
+    print "Interval: ", interval
+    if interval < 30:
+        target.write(buff.getvalue())
+        current += step
+        if step != orig_step:
+            step = int((1.5 * step) % orig_step)
+    else:
+        step /= 2
+    buff.close()
 
 target.close()
