98b1e0c90fb9b1ee29ee882c48b88e83b8646962
Alex Volkov <alex@flamy.ca>
2014-08-26 14:33:35 -0400

    Getting max remote ide automatically.

--- gmain_archive.py
+++ gmain_archive.py
@@ -9,38 +9,43 @@ import requests
 from time import time
 import codecs
 import StringIO
-maxget = 57370
+
+from get_latest import get_latest_remote_id, gmane_rss
+
+
 current = 0
 orig_step = 2000
-
-target = codecs.open('tlug_data.txt', 'a', 'utf-8')
-
 tlug_url = 'http://download.gmane.org/gmane.org.user-groups.linux.tolug'
-
 step = orig_step
 out = None
-while current <= maxget:
-    req_start = time()
-    buff = StringIO.StringIO()
-    dl_url = "%s/%d/%d" % (tlug_url, current, current + step % (maxget + 1))
-    print dl_url
-    del out
-    out = requests.get(dl_url)
-    if out.ok:
-        buff.write(out.text)
-    else:
-        print "Network error, retrying", out
-        continue
 
-    interval = time() - req_start
-    print "Interval: ", interval
-    if interval < 30:
-        target.write(buff.getvalue())
-        current += step
-        if step != orig_step:
-            step = int((1.5 * step) % orig_step)
-    else:
-        step /= 2
-    buff.close()
 
-target.close()
+if __name__ == '__main__':
+    maxget = get_latest_remote_id(gmane_rss)
+    target = codecs.open('tlug_data.txt', 'a', 'utf-8')
+
+    while current <= maxget:
+        req_start = time()
+        buff = StringIO.StringIO()
+        dl_url = "%s/%d/%d" % (tlug_url, current, current + step % (maxget + 1))
+        print dl_url
+        del out
+        out = requests.get(dl_url)
+        if out.ok:
+            buff.write(out.text)
+        else:
+            print "Network error, retrying", out
+            continue
+
+        interval = time() - req_start
+        print "Interval: ", interval
+        if interval < 30:
+            target.write(buff.getvalue())
+            current += step
+            if step != orig_step:
+                step = int((1.5 * step) % orig_step)
+        else:
+            step /= 2
+        buff.close()
+
+    target.close()
